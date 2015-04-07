a0d203bc1875cddf40c8a52c4db3524e4eeec1b2
Miguel Francisco <Miguel@DN800c3108.stanford.edu>
2015-04-06 21:35:50 -0700

    Testing new business logic for submit button

--- js/app.js
+++ js/app.js
@@ -409,7 +409,7 @@ function ShowQuestionForm() {
         $("#submitquestion"+i).fastClick(functionForResponse(i));
     }
     $("#app").append('<div id="other_answer"><textarea rows="1" cols="20" placeholder="Have another answer?!" id="custom_answer"></textarea></div><div id="other_submit">Submit</div>')
-    $('#other_submit').fastClick(functionForResponse(myDoc.pollCounts.length+1))
+    $('#other_submit').fastClick(functionForResponse(myDoc.pollCounts.length))
     $("#app").append('<img src="images/EGG-2.png" class="omlet_second"></img>');
 }
 
